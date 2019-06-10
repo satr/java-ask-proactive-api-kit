8fa48ab6165019f80b2708725b735b2023de8c20
satr <satr.github.io@gmail.com>
2019-06-10 18:44:20 +0200

    Added dependency import to jar.

--- sources/src/main/java/com/github/satr/ask/proactive/api/EnvironmentVariable.java
+++ sources/src/main/java/com/github/satr/ask/proactive/api/EnvironmentVariable.java
@@ -1,4 +1,5 @@
 package com.github.satr.ask.proactive.api;
+// Copyright © 2019, github.com/satr, MIT License
 
 public final class EnvironmentVariable {
     public static final String ApiEnvironment = "API_ENVIRONMENT";//"Dev" (default), "Prod"
